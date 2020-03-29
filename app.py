ba332438b8bce29d65ad332e479056a7d7f66c5c
1vanyk1 <57059188+1vanyk1@users.noreply.github.com>
2020-03-29 07:59:15 +0300

    Работает связь javascript и python

--- app.py
+++ app.py
@@ -44,37 +44,37 @@ def edit_user(x, y):
     return jsonify({'success': 'OK'})
 
 
-@app.route('/api/get_image', methods=['GET'])
-def edit_user():
-    d = drawSvg.Drawing(200, 100, origin='center')
-
-    d.append(drawSvg.Lines(-80, -45,
-                        70, -49,
-                        95, 49,
-                        -90, 40,
-                        close=False,
-                        fill='#eeee00',
-                        stroke='black'))
-
-    d.append(drawSvg.Rectangle(0, 0, 40, 50, fill='#1248ff'))
-    d.append(drawSvg.Circle(-40, -10, 30,
-                         fill='red', stroke_width=2, stroke='black'))
-
-    p = drawSvg.Path(stroke_width=2, stroke='green',
-                  fill='black', fill_opacity=0.5)
-    p.M(-30, 5)  # Start path at point (-30, 5)
-    p.l(60, 30)  # Draw line to (60, 30)
-    p.h(-70)  # Draw horizontal line to x=-70
-    p.Z()  # Draw line to start
-    d.append(p)
-
-    d.append(drawSvg.ArcLine(60, -20, 20, 60, 270,
-                          stroke='red', stroke_width=5, fill='red', fill_opacity=0.2))
-    d.append(drawSvg.Arc(60, -20, 20, 60, 270, cw=False,
-                      stroke='green', stroke_width=3, fill='none'))
-    d.append(drawSvg.Arc(60, -20, 20, 270, 60, cw=True,
-                      stroke='blue', stroke_width=1, fill='black', fill_opacity=0.3))
-    return d
+# @app.route('/api/get_image', methods=['GET'])
+# def edit_user():
+#     d = drawSvg.Drawing(200, 100, origin='center')
+#
+#     d.append(drawSvg.Lines(-80, -45,
+#                         70, -49,
+#                         95, 49,
+#                         -90, 40,
+#                         close=False,
+#                         fill='#eeee00',
+#                         stroke='black'))
+#
+#     d.append(drawSvg.Rectangle(0, 0, 40, 50, fill='#1248ff'))
+#     d.append(drawSvg.Circle(-40, -10, 30,
+#                          fill='red', stroke_width=2, stroke='black'))
+#
+#     p = drawSvg.Path(stroke_width=2, stroke='green',
+#                   fill='black', fill_opacity=0.5)
+#     p.M(-30, 5)  # Start path at point (-30, 5)
+#     p.l(60, 30)  # Draw line to (60, 30)
+#     p.h(-70)  # Draw horizontal line to x=-70
+#     p.Z()  # Draw line to start
+#     d.append(p)
+#
+#     d.append(drawSvg.ArcLine(60, -20, 20, 60, 270,
+#                           stroke='red', stroke_width=5, fill='red', fill_opacity=0.2))
+#     d.append(drawSvg.Arc(60, -20, 20, 60, 270, cw=False,
+#                       stroke='green', stroke_width=3, fill='none'))
+#     d.append(drawSvg.Arc(60, -20, 20, 270, 60, cw=True,
+#                       stroke='blue', stroke_width=1, fill='black', fill_opacity=0.3))
+#     return d
 
 
 @app.errorhandler(404)
